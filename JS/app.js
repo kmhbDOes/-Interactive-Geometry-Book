dc10f9a5c810b84c7182cb7ad89dc0110a5b827c
kmhbDOes <kmhbipul@gmail.com>
2023-02-18 10:31:12 +0600

    Rhombus and Pentagon card finished

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,7 @@ document.getElementById("btn-triangle").addEventListener('click', function () {
     const triangleBase = getInputFieldValue('triangle-base');
     const triangleHeight = getInputFieldValue('triangle-height');
 
-    if (isNaN(triangleBase) || isNaN(triangleHeight) || triangleBase < 1 || triangleHeight < 1) {
+    if (isNaN(triangleBase) || isNaN(triangleHeight) || triangleBase < 0 || triangleHeight < 0) {
         alert('Enter Value')
         return;
     }
@@ -28,7 +28,7 @@ document.getElementById("btn-rectangle").addEventListener('click', function () {
     const rectangleWidth = getInputFieldValue('rectangle-width');
     const rectangleLength = getInputFieldValue('rectangle-length');
 
-    if (isNaN(rectangleWidth) || isNaN(rectangleLength) || rectangleWidth < 1 || rectangleLength < 1) {
+    if (isNaN(rectangleWidth) || isNaN(rectangleLength) || rectangleWidth < 0 || rectangleLength < 0) {
         alert('Enter Value')
         return;
     }
@@ -46,7 +46,7 @@ document.getElementById("btn-parallelogram").addEventListener('click', function 
     const parallelogramBase = getInputFieldValue('parallelogram-base');
     const parallelogramHeight = getInputFieldValue('parallelogram-height');
 
-    if (isNaN(parallelogramBase) || isNaN(parallelogramHeight) || parallelogramBase < 1 || parallelogramHeight < 1) {
+    if (isNaN(parallelogramBase) || isNaN(parallelogramHeight) || parallelogramBase < 0 || parallelogramHeight < 0) {
         alert('Enter Value')
         return;
     }
@@ -61,10 +61,10 @@ document.getElementById("btn-parallelogram").addEventListener('click', function 
 document.getElementById("btn-rhombus").addEventListener('click', function () {
     serial += 1;
     const objectName = document.getElementById('rhombus-object').innerText;
-    const rhombusDiagonalOne = getInputFieldValue('rhombus-base');
-    const rhombusDiagonalTwo = getInputFieldValue('rhombus-height');
+    const rhombusDiagonalOne = getInputFieldValue('rhombus-d1');
+    const rhombusDiagonalTwo = getInputFieldValue('rhombus-d2');
 
-    if (isNaN(rhombusDiagonalOne) || isNaN(rhombusDiagonalTwo) || rhombusDiagonalOne < 1 || rhombusDiagonalTwo < 1) {
+    if (isNaN(rhombusDiagonalOne) || isNaN(rhombusDiagonalTwo) || rhombusDiagonalOne < 0 || rhombusDiagonalTwo < 0) {
         alert('Enter Value')
         return;
     }
@@ -73,5 +73,24 @@ document.getElementById("btn-rhombus").addEventListener('click', function () {
 
     const rhombusResult = 0.5 * rhombusDiagonalOne * rhombusDiagonalTwo;
     displayTableData(objectName, rhombusResult)
+})
+
+// Pentagon
+
+document.getElementById("btn-pentagon").addEventListener('click', function () {
+    serial += 1;
+    const objectName = document.getElementById('pentagon-object').innerText;
+    const pentagonPerimeter = getInputFieldValue('pentagon-perimeter');
+    const pentagonBase = getInputFieldValue('pentagon-base');
+
+    if (isNaN(pentagonPerimeter) || isNaN(pentagonBase) || pentagonPerimeter < 0 || pentagonBase < 0) {
+        alert('Enter Value')
+        return;
+    }
+
+    getTextValueById('pentagon-display');
+
+    const pentagonResult = 0.5 * pentagonPerimeter * pentagonBase;
+    displayTableData(objectName, pentagonResult)
 })
 
